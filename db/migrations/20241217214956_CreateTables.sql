4d8b29913fddb637b8c8a8164ce5cab8d85e0417
ryzhovalex <thed4rkof@gmail.com>
2024-12-18 01:29:02 +0300

    update sample.env, 20241217214956_CreateTables.sql, ...

--- db/migrations/20241217214956_CreateTables.sql
+++ db/migrations/20241217214956_CreateTables.sql
@@ -1,29 +1,81 @@
 -- migrate:up
-CREATE TABLE IF NOT EXISTS PersonType(
+CREATE TABLE PersonType(
     Id INTEGER PRIMARY KEY,
-    TypeKey VARCHAR(255),
+    TypeKey TEXT UNIQUE NOT NULL,
 );
 
-CREATE TABLE IF NOT EXISTS Person(
+CREATE TABLE Sponsorship(
     Id INTEGER PRIMARY KEY,
-    PersonType INTEGER,
-    FOREIGN KEY (PersonType) REFERENCES PersonType(id)
-
-    Firstname VARCHAR(255),
-    Surname VARCHAR(255),
-    BornDay INTEGER,
+    PersonId INTEGER NOT NULL,
+    FOREIGN KEY (PersonId) REFERENCES Person(Id),
+    SponsorId INTEGER NOT NULL,
+    FOREIGN KEY (SponsorId) REFERENCES Company(Id),
 );
 
-CREATE TABLE IF NOT EXISTS Timeline(
+CREATE TABLE Company(
+    Id INTEGER PRIMARY KEY,
+    CompanyName TEXT UNIQUE NOT NULL,
+    NetWorth FLOAT NOT NULL DEFAULT 0.0,
+);
+
+CREATE TABLE Person(
+    Id INTEGER PRIMARY KEY,
+    TypeId INTEGER,
+    FOREIGN KEY (TypeId) REFERENCES PersonType(Id),
+
+    Firstname TEXT NOT NULL,
+    Surname TEXT NOT NULL,
+    BornDay INTEGER NOT NULL,
+    -- Money earned from any sources: fights, sponsorships, side busineses.
+    TotalMoneyEarned FLOAT,
+);
+
+CREATE TABLE Timeline(
     Id INTEGER PRIMARY KEY,
     CurrentDay INTEGER,
 );
 
-CREATE TABLE IF NOT EXISTS WorldEvent(
+CREATE TABLE WorldEventType(
+    Id INTEGER PRIMARY KEY,
+    TypeKey TEXT UNIQUE NOT NULL,
+);
+
+CREATE TABLE WorldEvent(
     -- Id serve as event order.
     Id INTEGER PRIMARY KEY,
+    TypeId INTEGER NOT NULL,
+    FOREIGN KEY (TypeId) REFERENCES WorldEventType(Id),
     Body JSONB,
     TimelineDay INTEGER,
+);
+
+CREATE TABLE FightEndType(
+    Id INTEGER PRIMARY KEY,
+    TypeKey TEXT UNIQUE NOT NULL,
+);
+
+CREATE TABLE Fight(
+    Id INTEGER PRIMARY KEY,
+
+    Fighter0Id INTEGER NOT NULL,
+    FOREIGN KEY (Fighter0Id) REFERENCES Person(Id),
+    Fighter1Id INTEGER NOT NULL,
+    FOREIGN KEY (Fighter1Id) REFERENCES Person(Id),
+    RefereeId INTEGER NOT NULL,
+    FOREIGN KEY (RefereeId) REFERENCES Person(Id),
+    ArenaId INTEGER NOT NULL,
+    FOREIGN KEY (ArenaId) REFERENCES Person(Id),
+
+    OfflineWatchers INTEGER NOT NULL,
+    OnlineWatchers INTEGER NOT NULL,
+    MaxRounds INTEGER NOT NULL,
+    WinPrize FLOAT DEFAULT 0.0 NOT NULL,
+    -- Prize issued in any case to every fighter.
+    ParticipancePrize FLOAT,
+
+    -- If null: still going.
+    EndTypeId INTEGER,
+    FOREIGN KEY (EndTypeId) REFERENCES FightEndType(Id),
 );
 
 -- migrate:down
