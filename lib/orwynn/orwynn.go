e9f4248d4bbdc220f0befba1b34a2fd5f484bc26
ryzhovalex <thed4rkof@gmail.com>
2024-12-19 17:41:11 +0300

    update orwynn.go

--- lib/orwynn/orwynn.go
+++ lib/orwynn/orwynn.go
@@ -3,16 +3,22 @@ package orwynn
 
 import "worldboxing/lib/utils"
 
+const CodeCannotReferenceInnerTransport = 2
+
 type Code = utils.Code
 type Message struct {
 	Code Code `json:"Code"`
 	Body any  `json:"Body"`
 }
 type MessageContext struct {
-	Message      *Message
-	ConnectionId utils.Id
+	Message *Message
+	// AuthorConnection of message's author. Set to nil if author is the host.
+	AuthorConnection *Connection
+	// Connections to which the message should be sent to. Only considered for
+	// output messages.
+	TargetConnections []*Connection
 }
-type Subscriber func(*Message)
+type Subscriber func(*MessageContext)
 type Subscription struct {
 	Id         utils.Id
 	Code       Code
@@ -22,39 +28,66 @@ type Subscription struct {
 const MessageQueueMaxSize = 65536
 
 type State struct {
-	queue               *MessageQueue
-	transport           *Transport
-	codeToSubscriptions map[Code][]Subscription
+	// We have a single incoming and outcoming InputQueue for all transports.
+	InputQueue          *MessageQueue
+	OutputQueue         *MessageQueue
+	Transports          map[string]*Transport
+	CodeToSubscriptions map[Code][]*Subscription
 	// TODO: Eliminate risk of available subscription id overflow while
 	//		 earlier slots became vacant (by unsubscribing).
-	nextSubscriptionId utils.Id
+	NextSubscriptionId utils.Id
 }
 
 type PublishOptions struct {
-	TargetConnectionIds []utils.Id
+	TargetConnections []*Connection
 }
 
 type UnsubscribeFunction func()
 
 var state *State
 
-func Init(transport *Transport) *utils.Error {
+func Init(transports map[string]*Transport) *utils.Error {
+	_, ok := transports["INNER"]
+	if ok {
+		return utils.NewError(CodeCannotReferenceInnerTransport)
+	}
 	state = &State{
-		queue:     NewMessageQueue(MessageQueueMaxSize),
-		transport: transport,
+		InputQueue:          NewMessageQueue(MessageQueueMaxSize),
+		OutputQueue:         NewMessageQueue(MessageQueueMaxSize),
+		Transports:          transports,
+		CodeToSubscriptions: map[Code][]*Subscription{},
+		NextSubscriptionId:  0,
 	}
 	return nil
 }
 
+// Publish a message body to the bus.
+//
+// If options.TargetConnections is nil/empty, the inner transport will be used.
 func Publish(code Code, body any, options *PublishOptions) *utils.Error {
 	return nil
 }
 
 func Subscribe(code Code, subscriber Subscriber) (func(), *utils.Error) {
-	subscriptionId := state.nextSubscriptionId
-	state.nextSubscriptionId += 1
+	subscriptionId := state.NextSubscriptionId
+	state.NextSubscriptionId += 1
+	subscription := Subscription{
+		Id:         subscriptionId,
+		Code:       code,
+		Subscriber: subscriber,
+	}
+
+	_, ok := state.CodeToSubscriptions[code]
+	if !ok {
+		state.CodeToSubscriptions[code] = []*Subscription{}
+	}
+	state.CodeToSubscriptions[code] = append(
+		state.CodeToSubscriptions[code],
+		&subscription,
+	)
+
 	return func() {
-		subscriptions, ok := state.codeToSubscriptions[code]
+		subscriptions, ok := state.CodeToSubscriptions[code]
 		if !ok {
 			return
 		}
@@ -65,8 +98,8 @@ func Subscribe(code Code, subscriber Subscriber) (func(), *utils.Error) {
 				break
 			}
 		}
-		state.codeToSubscriptions[code] = utils.RemoveFromUnorderedSlice(
-			state.codeToSubscriptions[code],
+		state.CodeToSubscriptions[code] = utils.RemoveFromUnorderedSlice(
+			state.CodeToSubscriptions[code],
 			indexToDelete,
 		)
 	}, nil
